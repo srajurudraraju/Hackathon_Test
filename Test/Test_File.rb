9cb0c12f752d970fecdcf8bd1210003928dad9e9
srajurudraraju <60653456+srajurudraraju@users.noreply.github.com>
2020-02-04 19:59:44 +0530

    Update Test_File.rb

--- Test/Test_File.rb
+++ Test/Test_File.rb
@@ -1,5 +1,5 @@
 def sum_eq_n?(arr, n)
   return true .empty? && n == 0
   arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
-  error.New(fmt.Sprintf("parameter "))
+  error.New(fmt.Sprintf("parameter ")   )
 end
